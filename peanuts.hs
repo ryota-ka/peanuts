e1a63cf0f2a57ba2de2601464b9056c2c90070c7
Ryota Kameoka <kameoka.ryota@gmail.com>
2018-03-03 17:07:46 +0900

    Define <e, t> type predicates in Model PEANUTS

--- peanuts.hs
+++ peanuts.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Peanuts where
 
@@ -32,3 +33,52 @@ eval :: Expr model a -> a
 eval (EntityType e) = e
 eval (TruthValueType t) = t
 eval (CompositeType f) = f
+
+boy :: Expr Peanuts (Peanuts -> Bool)
+boy = CompositeType $ \case
+    Charlie   -> True
+    Linus     -> True
+    Schroeder -> True
+    _         -> False
+
+girl :: Expr Peanuts (Peanuts -> Bool)
+girl = CompositeType $ \case
+    Sally -> True
+    Lucy  -> True
+    Patty -> True
+    _     -> False
+
+dog :: Expr Peanuts (Peanuts -> Bool)
+dog = CompositeType $ \case
+    Snoopy -> True
+    _      -> False
+
+bird :: Expr Peanuts (Peanuts -> Bool)
+bird = CompositeType $ \case
+    Woodstock -> True
+    _         -> False
+
+player :: Expr Peanuts (Peanuts -> Bool)
+player = CompositeType $ \case
+    Snoopy    -> True
+    Charlie   -> True
+    Lucy      -> True
+    Linus     -> True
+    Patty     -> True
+    Schroeder -> True
+    _         -> False
+
+cute :: Expr Peanuts (Peanuts -> Bool)
+cute = CompositeType $ \case
+    Sally -> True
+    Lucy  -> True
+    Patty -> True
+    _     -> False
+
+crazy :: Expr Peanuts (Peanuts -> Bool)
+crazy = CompositeType $ const False
+
+counselor :: Expr Peanuts (Peanuts -> Bool)
+counselor = CompositeType $ \case
+    Lucy -> True
+    _    -> False
